098a80c6ed62aacb4efe5a1417a6c7eeec5655e8
Ivaylo Kostov <ikostov87@gmail.com>
2018-04-26 22:59:54 +0300

    code changes

--- 7.ArraysAndMatricesExercise/MagicMatrices.js
+++ 7.ArraysAndMatricesExercise/MagicMatrices.js
@@ -1,10 +1,6 @@
-function addPosition(arr) {
-    return arr
-        .filter((elem, index) => {return index % 2 !== 0})
-        .map(e => e * 2)
-        .reverse()
-        .join(' ')
-};
+function rework(){
+    
+}
 
 console.log(addPosition([10, 15, 20, 25]));
 console.log(addPosition([3, 0, 10, 4, 7, 3]));

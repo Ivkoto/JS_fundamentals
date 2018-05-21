4265eec17a5e674d77dbc76e71434c7bc4348ad3
Ivaylo Kostov <ikostov87@gmail.com>
2018-05-22 02:52:27 +0300

    Finished Orbit exercise

--- 7.ArraysAndMatricesExercise/Orbit.js
+++ 7.ArraysAndMatricesExercise/Orbit.js
@@ -1,19 +1,43 @@
-function makeOrbit(arr){
-    
-    let [width, height, x, y] = arr
+function makeOrbit(arr) {
 
-    let matrix = fillMatrixWithZeros(width, height);
-    matrix[x][y] = 1;
-    let nextNumber = 2;
+    let [rows, cols, x, y] = arr;
+    let matrix = fillMatrixWithZeros(rows, cols);
+    let nextNumber = 1;
+    let circle = 1;
+    matrix[x][y] = nextNumber;
 
-    for (let rowIdx = x; rowIdx < 4; rowIdx++) {
-        const element = matrix[rowIdx];
-        
+    while (!isMatrixFiled(matrix)) {
+        nextNumber++;
+        let topX = Math.max(0, x - circle);
+        let topY = Math.max(0, y - circle);
+        let bottomX = Math.min(rows - 1, x + circle);
+        let bottomY = Math.min(cols - 1, y + circle);
+
+        for (let rowIdx = topX; rowIdx <= bottomX; rowIdx++) {
+            for (let colIdx = topY; colIdx <= bottomY; colIdx++) {
+                if (matrix[rowIdx][colIdx] === 0) {
+                    matrix[rowIdx][colIdx] = nextNumber;
+                }
+            }
+        }
+        circle++;
     }
 
     printMatrix(matrix);
 
-    function printMatrix(matrix){
+    function isMatrixFiled(matrix) {
+        for (let rowIdx = 0; rowIdx < matrix.length; rowIdx++) {
+            for (let colIdx = 0; colIdx < matrix[rowIdx].length; colIdx++) {
+                let element = matrix[rowIdx][colIdx];
+                if (element === 0) {
+                    return false
+                }
+            }
+            return true;
+        }
+    }
+
+    function printMatrix(matrix) {
         console.log(matrix.map(e => e.join(' ')).join('\n'));
     }
 
@@ -26,5 +50,6 @@ function makeOrbit(arr){
     }
 }
 
-makeOrbit([5,5,2,2])
-makeOrbit([4,4,0,0])
+//makeOrbit([5, 5, 2, 2])
+//makeOrbit([4, 4, 0, 0])
+makeOrbit([5, 5, 0, 2])
